952da2b7d03cfcbd564d66e16b452ef10e1297c9
e <315731263315731263@qq.com>
2017-10-11 09:55:02 +0800

    修改本地配置

--- config/index.js
+++ config/index.js
@@ -48,7 +48,16 @@ module.exports = {
     // Proxy your API if using any.
     // Also see /build/script.dev.js and search for "proxy api requests"
     // https://github.com/chimurai/http-proxy-middleware
-    proxyTable: {}
+    proxyTable:
+       // proxy all requests starting with /api
+      '/api': {
+        target: 'http://10.0.100.119:8080/qidashi-boot/api',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    }
   }
 }
 
